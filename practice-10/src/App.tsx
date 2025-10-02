f917be7ce35c4d53154f8d4cb3916235111a8947
fabri10roman <fabri11roman.fr@gmail.com>
2025-10-02 02:08:03 -0300

    add exercise-3

--- practice-10/src/App.tsx
+++ practice-10/src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { Exercise1 } from "./pages/Exercise-1";
 import { Exercise2 } from "./pages/Exercise-2";
+import { Exercise3 } from "./pages/Exercise-3";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/exercise-1" element={<Exercise1 />} />
         <Route path="/exercise-2" element={<Exercise2 />} />
+        <Route path="/exercise-3" element={<Exercise3 />} />
       </Routes>
     </>
   );
